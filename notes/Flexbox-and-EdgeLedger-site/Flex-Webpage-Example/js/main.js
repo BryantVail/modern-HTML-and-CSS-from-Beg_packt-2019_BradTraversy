1b9ef7b4082c9159e331df5d4cb234fa3b8e9144
Bryant Vail <Bryanttv@outlook.com>
2019-07-07 00:53:40 -0400

    Flexbox complete

---
+++ notes/Flexbox-and-EdgeLedger-site/Flex-Webpage-Example/js/main.js
@@ -0,0 +1,58 @@
+//initialize and add the map
+function initMap(){
+    //your location (deland, fl)
+    const loc = { 
+        lat: 29.022030, 
+        lng: -81.341700
+    };
+    //Centered map on location
+    const map = new google.maps.Map(
+            document.querySelector(".map"),
+            {
+                zoom: 14,
+                center: loc 
+            }
+    );
+    //The Marker, positioned at location
+    const marker = new google.maps.Marker(
+        {
+            position: loc, 
+            map: map 
+        }
+    );
+}
+
+
+//smooth scrolling
+$("#navbar a, .btn").on("click", function(event){
+    //if # has a value, then prevent default function
+    if(this.hash !== ""){
+        event.preventDefault();
+
+        const hash = this.hash; 
+
+        $("html, body").animate(
+            {
+                scrollTop: $(hash).offset().top-125
+            },
+            800
+        );
+    }
+});
+
+window.addEventListener("scroll", function(){
+    if(this.window.scrollY > 25){
+        this.document.querySelector("#navbar")
+            .style
+            .opacity = 0.9;
+    }
+    else{
+        this.document.querySelector("#navbar")
+            .style
+            .opacity = 1;
+    }
+    
+});
+
+
+
